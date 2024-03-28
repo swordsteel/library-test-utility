6b839e12fa913c810f8f01384c1c4d61d9a7cd31
Mr Swordsteel <swordsteel@gmail.com>
2024-03-28 15:48:37 +0100

    use version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,29 @@
+@Suppress("UNUSED_PARAMETER")
+fun lulzRepository(repositoryHandler: RepositoryHandler) {
+    // TODO configuration for stored version catalogs and gradle plugins.
+//    repositoryHandler.maven {
+//        url = uri("https://")
+//        credentials {
+//            username =
+//            password =
+//        }
+//    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenLocal()
+        lulzRepository(this)
+    }
+    val catalog: String by settings
+    versionCatalogs.create("lulz").from("ltd.lulz.catalog:lulz-version:$catalog")
+}
+
 pluginManagement.repositories {
     mavenLocal()
     gradlePluginPortal()
-    // TODO configuration for stored gradle plugins
-    // maven {
-    //     url = uri("https://")
-    //     credentials {
-    //         username =
-    //         password =
-    //     }
-    // }
+    lulzRepository(this)
 }
 
 rootProject.name = "test-utility"
